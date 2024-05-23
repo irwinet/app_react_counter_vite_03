fea1c620eb5eada5f466b8a3e1f139d3e50701bf
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-23 18:28:18 -0500

    Use Styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 import { FirstApp } from './FirstApp'
 // import { App } from './HelloWorldApp'
 // import App from './HelloWorldApp'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
