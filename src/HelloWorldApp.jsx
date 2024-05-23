b95c659a51834722a1ecdacab614c0a35f2d4371
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-23 00:54:02 -0500

    Add component HelloWorldApp

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+
+// export function App() {
+//     return (<h1>Hola Mundo!!!</h1>);
+// }
+
+// export default App;
+
+// import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
