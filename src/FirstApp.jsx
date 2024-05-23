740354de2ce5d1adc6d265e3265d778bbde626db
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-23 01:27:07 -0500

    Add Fragment

---
+++ src/FirstApp.jsx
@@ -0,0 +1,19 @@
+// import { Fragment } from 'react'
+
+// export const FirstApp = () => {
+//   return (
+//     <Fragment>
+//         <h1>Irwin</h1>
+//         <p>Soy un subtitulo</p>
+//     </Fragment>
+//   )
+// }
+
+export const FirstApp = () => {
+    return (
+        <>
+            <h1>Irwin</h1>
+            <p>Soy un subtitulo</p>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HelloWorldApp } from './HelloWorldApp'
+// import { HelloWorldApp } from './HelloWorldApp'
+import { FirstApp } from './FirstApp'
 // import { App } from './HelloWorldApp'
 // import App from './HelloWorldApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>
 )
