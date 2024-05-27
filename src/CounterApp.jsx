fc4673b24689cd91fd6b0bf4d57a06d7a4ac0dd7
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-27 14:30:26 -0500

    Add Hook useSate

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,26 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export const CounterApp = ({ value }) => {
+
+  const [counter, setCounter] = useState(value);
+
+  // eslint-disable-next-line no-unused-vars
+  const handleAdd = (event) => {
+    // console.log(event)
+    setCounter(counter+1);
+    // setCounter((c) => c+1);
+  }  
+
   return (
     <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
+        <button onClick={ handleAdd }>
+            +1
+        </button>
     </>
   )
 }
