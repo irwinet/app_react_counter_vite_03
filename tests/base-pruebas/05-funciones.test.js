b2e2a38e04136595fe985c7bd970f003c24b8269
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-28 11:24:20 -0500

    Add test 05-funciones.test

---
+++ tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
+
+describe('Prubeas en 05-funciones', () => { 
+    test('getUser debe de retornar un objeto', () => {
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+
+        const user = getUser();
+        
+        expect(testUser).toEqual(user);
+    });
+
+    test('getUsuarioActivo debe retornar un objeto', () => {
+        const name = 'Irwin';
+
+        const user = getUsuarioActivo(name);
+        
+        expect(user).toStrictEqual({
+            uid: 'ABC567',
+            username: name
+        });
+    });
+});
