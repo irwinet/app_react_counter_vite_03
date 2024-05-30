f5080c4730ea64727cb4807d207b6c437fe613ba
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-30 18:00:58 -0500

    Add test FirstApp Component

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -7,6 +7,19 @@ describe('Pruebas en <FirstApp />', () => {
     test('Debe de hacer match con el snapshot', () => {
         const title = "Titulo"
         const subTitle = 123
-        render(<FirstApp title={ title } subTitle= { subTitle } />);
+        const { container } = render(<FirstApp title={ title } subTitle= { subTitle } />);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test("Debe de mostrar el titulo en un h1", () => {
+        const title = "Titulo"
+        const subTitle = 123
+        const { container, getByText } = render(<FirstApp title={ title } subTitle= { subTitle } />);
+
+        expect(getByText(title)).toBeTruthy();
+        const h1 = container.querySelector('h1');
+        // expect(h1.innerHTML).toBe(title);
+        expect(h1.innerHTML).toContain(title);
     });
 });
