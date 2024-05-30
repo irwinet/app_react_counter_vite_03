ad39d69e1a09c1d1fbbfcc73d722fddf6d3450f8
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-30 11:53:49 -0500

    Add test 09-promesas

---
+++ tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+
+import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
+
+describe("Pruebas en 09-promesas", () => {
+    test("getHeroeByIdAsync debe de retornar un heroe", (done) => {
+        const id = 1;
+        getHeroeByIdAsync(id)
+            .then((heroe) => {
+                expect(heroe).toEqual({
+                    id: 1,
+                    name: 'Batman',
+                    owner: 'DC'
+                });
+                done();
+            });
+    });
+
+    test("getHeroeByIdAsync debe de retornar un error si heroe no existe", (done) => {
+        const id = 100;
+        getHeroeByIdAsync(id)
+            .then(heroe => {
+                expect(heroe).toBeFalsy();
+                done();
+            })
+            .catch((error) => {
+                expect(error).toBe('No se pudo encontrar el héroe 100');
+                done();
+            });
+    });
+});
