b4add797408883d15779620690a3c6a178760492
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-31 14:52:58 -0500

    Add test at CounterApp.test

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { HelloWorldApp } from './HelloWorldApp'
-import { FirstApp } from './FirstApp'
+// import { FirstApp } from './FirstApp'
 // import { App } from './HelloWorldApp'
 // import App from './HelloWorldApp'
-// import { CounterApp } from './CounterApp'
+import { CounterApp } from './CounterApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title='Titulo' subTitle={123} />
-        {/* <CounterApp value={0} /> */}
+        {/* <FirstApp title='Titulo' subTitle={123} /> */}
+        <CounterApp value={0} />
     </React.StrictMode>
 )
---
+++ tests/CounterApp.test.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+
+import { render, screen } from "@testing-library/react";
+import { CounterApp } from "../src/CounterApp";
+
+describe('Pruebas en CounterApp', () => {
+    const value = 100; 
+    test('Debe de hacer match con el snapshot', () => {
+        const { container } = render(<CounterApp value={value} />);
+        expect(container).toMatchSnapshot();
+    });
+
+    test('Debe de mostrar el valor inicial de 100', () => {
+        render(<CounterApp value={value} />);
+        expect(screen.getByText(value)).toBeTruthy();   
+        expect(screen.getByText('-1')).toBeTruthy();
+        
+        // expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(" 100 ");
+    });
+});
